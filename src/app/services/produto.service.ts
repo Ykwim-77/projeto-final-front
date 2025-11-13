5d6e6ef6e6dea16a1fa51cb10c7638e56cd1aa77
rafaelpilonetto <rafaelpilonetto59@gmail.com>
2025-11-12 22:06:07 -0300

    0.8.009

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 
 export interface Produto {
+  codigo_publico: any;
   sku: any;
   id_patrimonio: number;
   preco_unitario: number | undefined;
